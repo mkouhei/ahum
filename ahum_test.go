ff553cf8428e1bd06a7e7679e84da007542b3477
Kouhei Maeda <mkouhei@palmtb.net>
2014-08-17 07:32:53 +0900

    Added fail case of connecting to mqtt broker.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- ahum_test.go
+++ ahum_test.go
@@ -1,15 +1,14 @@
 package ahum
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestConn(t *testing.T) {
-	c := &config{"localhost", 1883, "ahummq", "guest", "passw0rd", 1, 3, true, 10}
-	r, err := c.Conn()
-	if err != nil {
-		t.Fatal(err)
+	c := &config{"localhost", 1882, "ahummq", "guest", "passw0rd", 1, 3, true, 10}
+	if r, err := c.Conn(); err == nil || r != -1 {
+		t.Fatalf("%v, rc: %v, want: dial tcp 127.0.0.1:1882: connection refused", err, r)
 	}
-	fmt.Println(r)
+	c = &config{"localhost", 1883, "ahummq", "guest", "passw0rd", 1, 3, true, 10}
+	if r, err := c.Conn(); err != nil || r != 59 {
+		t.Fatalf("%v, rc: %v, want: 59", err, r)
+	}
 }
